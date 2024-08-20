54e97f026251bb5561a18d82cf2df64afa095826
akallbold <a.kallenbornbolden@gmail.com>
2024-08-19 20:51:57 -0400

    Initial Commit

---
+++ retrograde/retrogradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  retrogradeApp.swift
+//  retrograde
+//
+//  Created by Anna Kallenborn-Bolden on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct retrogradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
